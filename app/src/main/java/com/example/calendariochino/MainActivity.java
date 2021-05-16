b94b3d8f1a09970edb672e448850ec1fde67ac7e
Fran <frankzzcsgo@gmail.com>
2021-05-16 12:35:25 +0200

    added maps

--- app/src/main/java/com/example/calendariochino/MainActivity.java
+++ app/src/main/java/com/example/calendariochino/MainActivity.java
@@ -58,4 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void MapsActivity (View view){
+
+        Intent myIntent;
+
+        myIntent = new Intent(view.getContext(), MapsActivity.class);
+
+        startActivity(myIntent);
+
+    }
+
 }
